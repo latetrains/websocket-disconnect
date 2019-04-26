c954b560eb0f625ce4ff3926e0ed568eff6e9deb
Richard Hughes <eeuc40@hotmail.com>
2019-04-26 21:03:48 +0100

    Use env variables

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 let AWS = require('aws-sdk');
 let dynamo = new AWS.DynamoDB.DocumentClient();
 
-const tableName = 'Latetrains';
 const OK = 200;
+const INTERNAL_SERVER_ERROR = 500;
 
 exports.handler = async (event) => {
     const connectionId = event.requestContext.connectionId;
 
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         this.removeItemFromDynamo({
             connectionid: connectionId
         }).then(() => {
             resolve({
                 statusCode: OK
             });
+        }).catch(() => {
+            reject({
+                statusCode: INTERNAL_SERVER_ERROR
+            })
         });
     });
 };
 
 exports.removeItemFromDynamo = item => {
     return dynamo.delete({
-        TableName: tableName,
+        TableName: process.env.TABLE_NAME,
         Key: item,
     }).promise();
 };
